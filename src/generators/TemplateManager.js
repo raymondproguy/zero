e0c2259363a7ffa4a31888e88d383c4a57682e38
raymondproguy <raymondnicholas833@gmail.com>
2025-11-11 17:07:04 +0100

    fix: fixed template path issues

--- src/generators/TemplateManager.js
+++ src/generators/TemplateManager.js
@@ -17,11 +17,11 @@ export class TemplateManager {
     let templateType = 'base';
     
     if (this.options.auth && this.options.db) {
-      templateType = `auth-${this.options.db}`;
+      templateType = `auth-enabled-${this.options.db}`;
     } else if (this.options.auth) {
       templateType = 'auth-enabled';
     } else if (this.options.db) {
-      templateType = `db-${this.options.db}`;
+      templateType = `db-enabled/${this.options.db}`;
     }
     
     const language = this.options.language === 'TypeScript' ? 'typescript' : 'javascript';
